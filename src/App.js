bb93f39487075bb6492856f36b3ee762820612dc
Bryce Hoover <bryhoover11@gmail.com>
2021-07-12 16:28:28 -0500

    Added toggle for reminder to JSON server and app.js

--- src/App.js
+++ src/App.js
@@ -33,6 +33,14 @@ const fetchTasks = async() => {
   return data
 }
 
+const fetchTask = async(id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+
+  return data
+}
+
+
 //Add Task
 const addTask = async(task) => {
   const res = await fetch('http://localhost:5000/tasks', {
@@ -66,8 +74,21 @@ const deleteTask = async(id) => {
 };
 
 //Toggle Reminder 
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+const toggleReminder = async(id) => {
+  const toggleTask = await fetchTask(id)
+  const updatedTask = { ...toggleTask, reminder: !toggleTask.reminder } 
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+    'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updatedTask)
+  })
+
+  const data = await res.json()
+
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task))
 }
 
   return (

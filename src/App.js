50a1a8e4b44e599cf3c1e98f338de6b656855bbf
Bryce Hoover <bryhoover11@gmail.com>
2021-07-12 15:28:46 -0500

    Add new task route added

--- src/App.js
+++ src/App.js
@@ -34,10 +34,25 @@ const fetchTasks = async() => {
 }
 
 //Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = { id, ...task }
-  setTasks([...tasks, newTask])
+const addTask = async(task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  //saves data from res.json()
+  const data = await res.json()
+
+  //spreads existing tasks and adds data from res.json()
+  setTasks([...tasks, data])
+
+
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = { id, ...task }
+  // setTasks([...tasks, newTask])
 }
 
 //Delete Task

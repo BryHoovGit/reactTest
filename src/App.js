8ce46a7ae9392d87cdc6f77c71910735f324d1d2
Bryce Hoover <bryhoover11@gmail.com>
2021-07-12 18:57:03 -0500

    Added React-Router-DOM & routes, added about & footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
+
+import { useLocation } from 'react-router-dom'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 
 //Comes from Tasks.js file originally
 // Use effect is a hook used to create side effects when page loads
@@ -92,11 +97,20 @@ const toggleReminder = async(id) => {
 }
 
   return (
+    <Router> 
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
+      
+      <Route path='/' exact render={(props) => (
+        <>
+          {showAddTask && <AddTask onAdd={addTask}/>}
+          {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
+        </>
+      )} />
+      <Route path='/about' component={About} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@
 //  rafce = quick React function snippet
 
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Button from './Button';
 
 //Styling can be done inline, but needs double curly braces
@@ -15,10 +16,11 @@ import Button from './Button';
 // }
 
 const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+            {location.pathname === '/' && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />}
         </header>
     )
 }
